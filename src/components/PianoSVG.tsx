932b9c44e8a19ab6d8dbfcbefabad080fef69926
a <a@pop-os.localdomain>
2023-12-27 20:43:15 +0100

    Piano black keys better position

--- src/components/PianoSVG.tsx
+++ src/components/PianoSVG.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import SVGPianoKey from "./SVGPianoKey";
+import SVGPianoKey, { WhitePianoKey } from "./SVGPianoKey";
+
 
 const PianoSVG = () => {
   const whiteHeight = 16;
@@ -9,24 +10,27 @@ const PianoSVG = () => {
     <div className="w-1/3 h-96">
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 34">
         <g id="Keyboard">
-          <g id="Octave" stroke="#111" stroke-width="0.125">
+          <g id="Octave" stroke="#111" strokeWidth="0.125">
             <g id="WhiteKeys" fill="#eee">
-              <SVGPianoKey x={2} y={2} width={4} height={whiteHeight} />
-              <SVGPianoKey x={2} y={2} width={4} height={whiteHeight} />
+              <WhitePianoKey i={0} />
+              <WhitePianoKey i={1} />
+              <WhitePianoKey i={2} />
+              <WhitePianoKey i={3} />
+              {/* <SVGPianoKey x={2} y={2} width={4} height={whiteHeight} />
               <SVGPianoKey x={6} y={2} width={4} height={whiteHeight} />
               <SVGPianoKey x={10} y={2} width={4} height={whiteHeight} />
-              <SVGPianoKey x={14} y={2} width={4} height={whiteHeight} />
+              <SVGPianoKey x={14} y={2} width={4} height={whiteHeight} /> */}
               <SVGPianoKey x={18} y={2} width={4} height={whiteHeight} />
               <SVGPianoKey x={22} y={2} width={4} height={whiteHeight} />
               <SVGPianoKey x={26} y={2} width={4} height={whiteHeight} />
             </g>
 
             <g id="BlackKeys" fill="#000">
-              <SVGPianoKey x={4.125} y={2} width={2.5} height={blackHeight} />
-              <SVGPianoKey x={9.25} y={2} width={2.5} height={blackHeight} />
-              <SVGPianoKey x={16.125} y={2} width={2.5} height={blackHeight} />
+              <SVGPianoKey x={4.75} y={2} width={2.5} height={blackHeight} />
+              <SVGPianoKey x={8.75} y={2} width={2.5} height={blackHeight} />
+              <SVGPianoKey x={16.75} y={2} width={2.5} height={blackHeight} />
               <SVGPianoKey x={20.75} y={2} width={2.5} height={blackHeight} />
-              <SVGPianoKey x={25.25} y={2} width={2.5} height={blackHeight} />
+              <SVGPianoKey x={24.75} y={2} width={2.5} height={blackHeight} />
             </g>
           </g>
         </g>
--- src/components/SVGPianoKey.tsx
+++ src/components/SVGPianoKey.tsx
@@ -1,9 +1,36 @@
+"use client";
+import React, { useContext, useEffect } from "react";
+import { KeyToNote, NoteName, NoteToKey } from "@/lib/music_theory";
+import { StateContext } from "@/app/context/StateContext";
+
+type PianoKeyProps = {
+  i: number;
+};
+
+export const WhitePianoKey = ({ i }: PianoKeyProps) => {
+  // Use context
+  const { state, dispatch } = useContext(StateContext)!;
+
+  const noteNames: NoteName[] = Object.values(NoteName);
+  const noteName = noteNames[i];
+  const isActive = state.pressedKeys[NoteToKey[noteName]] || false;
+  const x = i * 4 + 2;
+  const y = 2;
+  const w = 4;
+  const h = 16;
+  return (
+    <g key={i} fill={isActive ? "#CE7B91" : "white"}>
+      <SVGPianoKey x={x} y={y} width={w} height={h} />
+    </g>
+  )
+};
+
 type SVGPianoKeyProps = {
-  x: number
-  y: number
-  width: number
-  height: number
-}
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+};
 
 export default function SVGPianoKey({ x, y, width, height }: SVGPianoKeyProps) {
   return (
