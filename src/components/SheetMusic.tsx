8c8e20d9e52d18738483f67f1886529c318a0c43
a <a@pop-os.localdomain>
2024-01-08 16:18:39 +0100

    Two staves next to each other

--- src/components/SheetMusic.tsx
+++ src/components/SheetMusic.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import { toStaveNote } from "@/app/sheet_music";
-import { Bar } from "@/app/state/music";
-import { Formatter, Stave, Voice, Vex } from "vexflow";
+import { Bar, Duration, MusicalEvent, Note } from "@/app/state/music";
+import { Formatter, Stave, Voice, Vex, RenderContext } from "vexflow";
 
 const { Renderer } = Vex.Flow;
 
@@ -28,12 +28,6 @@ function createRenderContext(containerId: string, outputId: string) {
   return renderer.getContext();
 }
 
-function createStave() {
-  const [x, y] = [0, STAVE_HEIGHT];
-  const stave = new Stave(x, y, STAVE_WIDTH);
-  return stave;
-}
-
 function displayError(e: any): void {
   const errorDiv = document.getElementById(DIV_ID.ERROR);
   if (errorDiv) {
@@ -41,44 +35,102 @@ function displayError(e: any): void {
   }
 }
 
-function Canvas({ children }: { children: React.ReactNode }) {
+export function Canvas() {
   const containerRef = useRef(null);
+  const renderContextRef = useRef<RenderContext | null>(null);
+
+  // Test data
+  const bars: Bar[] = [
+    {
+      timeSignature: "4/4",
+      events: [
+        {
+          notes: [Note.D, Note.E, Note.G],
+          duration: Duration.Quarter,
+        },
+        {
+          notes: [Note.C],
+          duration: Duration.Quarter,
+        },
+        {
+          notes: [Note.C, Note.E, Note.G],
+          duration: Duration.Quarter,
+        },
+        {
+          notes: [Note.C, Note.E, Note.G],
+          duration: Duration.Quarter,
+        },
+      ],
+    },
+  ];
+
+  useEffect(() => {
+    const context = createRenderContext(DIV_ID.CONTAINER, DIV_ID.OUTPUT);
+    renderContextRef.current = context;
+  }, [containerRef.current]);
 
   return (
-    <div>
-      <div id={DIV_ID.ERROR}></div>
-      <div id={DIV_ID.CONTAINER} ref={containerRef}>
-        <div id={DIV_ID.OUTPUT}></div>
-      </div>
-    </div>
+    <>
+      {renderContextRef ? (
+        <>
+          <div id={DIV_ID.ERROR}></div>
+          <div id={DIV_ID.CONTAINER} ref={containerRef}>
+            <div id={DIV_ID.OUTPUT}>f</div>
+          </div>
+        </>
+      ) : (
+        <>Loading...</>
+      )}
+    </>
   );
+}
+
+function createStave(events: MusicalEvent[], x: number): Stave {
+  const y = STAVE_HEIGHT;
+  return new Stave(x, y, STAVE_WIDTH);
+}
+
+function createVoice(events: MusicalEvent[]): Voice {
+  const notes = events.map(toStaveNote);
+  const voice = new Voice({ num_beats: 4, beat_value: 4 });
+  voice.addTickables(notes);
+  return voice;
+}
+
+function draw(context: RenderContext, stave: Stave, voice: Voice): void {
+  try {
+    new Formatter().joinVoices([voice]).formatToStave([voice], stave);
+    stave.setContext(context).draw();
+    voice.draw(context, stave);
+  }
+  catch (e) {
+    displayError(e);
+  }
 }
 
 export default function SheetMusic({ timeSignature, events }: Bar) {
   const containerRef = useRef(null);
-  const [on, setOn] = useState(false);
-
-  function addStave() {
-
-  }
+  const renderContextRef = useRef<RenderContext | null>(null);
 
   useEffect(() => {
     const context = createRenderContext(DIV_ID.CONTAINER, DIV_ID.OUTPUT);
-    const stave = createStave();
-    const notes = events.map(toStaveNote);
-    const voice = new Voice({ num_beats: 4, beat_value: 4 });
-    voice.addTickables(notes);
+    renderContextRef.current = context;
+    
+    const testEvent = {
+      notes: [Note.C],
+      duration: Duration.Whole,
+    };
 
-    try {
-      new Formatter().joinVoices([voice]).formatToStave([voice], stave);
+    const x = 0;
+    const stave = createStave(events, x);
+    const voice = createVoice(events);
 
-      stave.setContext(context).draw();
-      voice.draw(context, stave);
-    } catch (e) {
-      displayError(e);
-    }
-
-    setOn(true);
+    const x2 = stave.getWidth();
+    const stave2 = createStave([testEvent], x2);
+    const voice2 = createVoice([testEvent]);
+    
+    draw(context, stave, voice);
+    draw(context, stave2, voice2);
 
     return () => {
       [DIV_ID.OUTPUT, DIV_ID.ERROR].forEach((id) => {
@@ -90,15 +142,21 @@ export default function SheetMusic({ timeSignature, events }: Bar) {
     };
   }, [containerRef.current]);
 
-  // useEffect(() => {
-  //   console.log("turned on");
-  // }, [on]);
-
   return (
     <div>
       <div id={DIV_ID.ERROR}></div>
       <div id={DIV_ID.CONTAINER} ref={containerRef}>
         <div id={DIV_ID.OUTPUT}></div>
+        {/* <div>
+          {events.map((event, i) => (
+            <div key={i}>
+              {event.notes.map((note, j) => (
+                <span key={j}>{note}</span>
+              ))}
+              <span>{event.duration}</span>
+            </div>
+          ))}
+        </div> */}
       </div>
     </div>
   );
