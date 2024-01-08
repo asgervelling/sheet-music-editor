7396069d713d30ed92252353d2aa0930e889093e
a <a@pop-os.localdomain>
2024-01-08 15:42:05 +0100

    whoops

--- src/components/SheetMusic.tsx
+++ src/components/SheetMusic.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useRef } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect, useRef, useState } from 'react';
 import { toStaveNote } from "@/app/sheet_music";
 import { Bar } from "@/app/state/music";
 import { Formatter, Stave, Voice, Vex } from "vexflow";
@@ -15,9 +14,14 @@ enum DIV_ID {
   ERROR = "error",
 }
 
-function createRenderContext() {
-  const renderer = new Renderer(DIV_ID.OUTPUT, Renderer.Backends.SVG);
-  const parentDiv = document.getElementById(DIV_ID.CONTAINER);
+/**
+ * Create a VexFlow renderer and return its context.
+ * @param containerId The ID of the container element.
+ * @param outputId The ID of the output element.
+ */
+function createRenderContext(containerId: string, outputId: string) {
+  const renderer = new Renderer(outputId, Renderer.Backends.SVG);
+  const parentDiv = document.getElementById(containerId);
   const parentWidth = parentDiv ? parentDiv.offsetWidth : 500;
   const parentHeight = parentDiv ? parentDiv.offsetHeight : 200;
   renderer.resize(parentWidth, parentHeight);
@@ -37,11 +41,29 @@ function displayError(e: any): void {
   }
 }
 
-export default function SheetMusic({ timeSignature, events }: Bar) {
+function Canvas({ children }: { children: React.ReactNode }) {
   const containerRef = useRef(null);
 
+  return (
+    <div>
+      <div id={DIV_ID.ERROR}></div>
+      <div id={DIV_ID.CONTAINER} ref={containerRef}>
+        <div id={DIV_ID.OUTPUT}></div>
+      </div>
+    </div>
+  );
+}
+
+export default function SheetMusic({ timeSignature, events }: Bar) {
+  const containerRef = useRef(null);
+  const [on, setOn] = useState(false);
+
+  function addStave() {
+
+  }
+
   useEffect(() => {
-    const context = createRenderContext();
+    const context = createRenderContext(DIV_ID.CONTAINER, DIV_ID.OUTPUT);
     const stave = createStave();
     const notes = events.map(toStaveNote);
     const voice = new Voice({ num_beats: 4, beat_value: 4 });
@@ -56,6 +78,8 @@ export default function SheetMusic({ timeSignature, events }: Bar) {
       displayError(e);
     }
 
+    setOn(true);
+
     return () => {
       [DIV_ID.OUTPUT, DIV_ID.ERROR].forEach((id) => {
         const div: HTMLElement | null = document.getElementById(id);
@@ -65,6 +89,10 @@ export default function SheetMusic({ timeSignature, events }: Bar) {
       });
     };
   }, [containerRef.current]);
+
+  // useEffect(() => {
+  //   console.log("turned on");
+  // }, [on]);
 
   return (
     <div>
