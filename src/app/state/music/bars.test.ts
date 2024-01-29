5ce0887f7077822f17b0d12bc8b458141daa8184
a <a@pop-os.localdomain>
2024-01-29 11:07:40 +0100

    Fixed sth

--- src/app/state/music/bars.test.ts
+++ src/app/state/music/bars.test.ts
@@ -17,21 +17,24 @@ import {
   e2t,
   e8t,
   fmtChunk,
+  fmtChunks,
   p4,
   p8,
 } from "./test_helpers";
 import { TimeSignature } from "./time_signatures";
-import { repeat } from "./arrays";
+import { head, repeat } from "./arrays";
+import { MusicalEvent } from ".";
 
 const _3_4: TimeSignature = [3, D.Quarter];
 const _3_16: TimeSignature = [3, D.Sixteenth];
 
 function fmtBars(bars: Bar[]) {
-  return bars.map((bar) => fmtChunk(bar.events)).join(" | ");
+  return fmtChunks(bars.map((b) => b.events));
 }
 
 describe("createBars", () => {
   it("should fill the last bar with pauses", () => {
+    console.log(fmtBars(createBars([c1, e1], [3, D.Quarter])))
     expect(createBars([c1, e1], [3, D.Quarter])).toEqual([
       {
         ts: _3_4,
@@ -60,6 +63,7 @@ describe("createBars", () => {
   });
 
   it("should split multiple events over multiple bars", () => {
+    console.log((createBars([c1, e1t, e1t, e1], _3_16)))
     expect(createBars([c1, e1t, e1t, e1], _3_16)).toEqual([
       ...repeat({ ts: _3_16, events: [c8t, c16t] }, 5),
       { ts: _3_16, events: [c16, e8t] },
--- src/app/state/music/bars.ts
+++ src/app/state/music/bars.ts
@@ -1,7 +1,5 @@
 import { first, last } from "./arrays";
-import { Duration } from "./durations";
 import { MusicalEvent, chunk, expandTo32nds, reciprocalChunk } from "./events";
-import { c4, e2 } from "./test_helpers";
 import { TimeSignature, tsTo32nds } from "./time_signatures";
 
 /**
@@ -25,10 +23,9 @@ export function createBars(events_: MusicalEvent[], ts: TimeSignature): Bar[] {
   const numBars = Math.ceil(total32nds / chunkSize);
   const chunkSizes = Array(numBars).fill(chunkSize);
   const chunks = chunk(events_, chunkSizes);
-  const lastChunk = [...last(chunks), ...reciprocalChunk(last(chunks), chunkSize)]
-  const bars = [
+  return [
     ...first(chunks),
     [...last(chunks), ...reciprocalChunk(last(chunks), chunkSize)],
   ].map((events) => ({ ts, events }));
-  return bars;
+
 }
