944fea68be0ac3663f973a48ee44971a002d9028
a <a@pop-os.localdomain>
2024-02-04 21:59:55 +0100

    Documented arrays.ts more

--- src/app/state/music/arrays.ts
+++ src/app/state/music/arrays.ts
@@ -1,13 +1,20 @@
-export const head = <T>(l: T[]) => l[0];
-export const tail = <T>(l: T[]) => l.slice(1);
+/** Grab the first item of an array. */
+export const head = <T>(l: T[]): T => l[0];
 
-export const last = <T>(l: T[]) => l[l.length - 1];
-export const first = <T>(l: T[]) => l.slice(0, -1);
+/** Return the last n-1 items of `l`, or `[]`. */
+export const tail = <T>(l: T[]): T[] => l.slice(1);
+
+/** Grab the last element of an array. */
+export const last = <T>(l: T[]): T => l[l.length - 1];
+
+/** Return the first n-1 items of `l`, or `[]`. */
+export const first = <T>(l: T[]): T[] => l.slice(0, -1);
 
 export const arrayEquals = <T>(l: T[], m: T[]): boolean => {
   return l.length === m.length && l.every((e, i) => e === m[i]);
 };
 
+/** Create an array of `n` `x`'s. */
 export function repeat<T>(x: T, n: number): T[] {
   if (n <= 0 || isNaN(n)) return [];
   function createArray<T>(a: T[], n: number) {
@@ -18,8 +25,9 @@ export function repeat<T>(x: T, n: number): T[] {
   return createArray([x], n);
 }
 
+/** Split `l` into chunks of sizes `chunkSizes`. */
 export function chunk<T>(l: T[], chunkSizes: number[]): T[][] {
-  if (chunkSizes.length === 0) return []
+  if (chunkSizes.length === 0) return [];
   const n = head(chunkSizes);
   const [c, rest] = [l.slice(0, n), l.slice(n)];
   return [c, ...chunk(rest, tail(chunkSizes))];
@@ -40,9 +48,9 @@ export function rotate<T>(l: T[], n: number): T[] {
 
 /**
  * Create an array of pairs from `l` like dominoes, e.g.
- * 
+ *
  *   `pair([1, 2, 3]) = [[1, 2], [2, 3]]`
- * 
+ *
  * If `l` has a length of 1, the first and only pair
  * will have a first element of `null`.
  */
