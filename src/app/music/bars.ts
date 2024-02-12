89a7e5dc03a55f636354ea63e06e3593c8c2f574
a <a@pop-os.localdomain>
2024-02-12 10:41:22 +0100

    Added new function signature: setTimeSignature

--- src/app/music/bars.ts
+++ src/app/music/bars.ts
@@ -2,6 +2,7 @@ import { first, last } from "./arrays";
 import { chunk, expandTo32nds, reciprocalChunk } from "./events";
 import { TimeSignature, tsTo32nds } from "./time_signatures";
 import { MusicalEvent, NoteName, Clef } from ".";
+
 /**
  * A bar has a time signature,
  * a key signature and some musical events.
@@ -18,21 +19,47 @@ export type Bar = {
  * If the last bar is missing some events, add pauses to it.
  */
 export function createBars(
-  events_: MusicalEvent[],
+  events: MusicalEvent[],
   clef: Clef,
   timeSig: TimeSignature,
   keySig: NoteName
 ): Bar[] {
-  if (events_.length === 0) {
+  if (events.length === 0) {
     return [];
   }
   const chunkSize = tsTo32nds(timeSig).length;
-  const total32nds = events_.flatMap(expandTo32nds).length;
+  const total32nds = events.flatMap(expandTo32nds).length;
   const numBars = Math.ceil(total32nds / chunkSize);
   const chunkSizes = Array(numBars).fill(chunkSize);
-  const chunks = chunk(events_, chunkSizes);
+  const chunks = chunk(events, chunkSizes);
   return [
     ...first(chunks),
     [...last(chunks), ...reciprocalChunk(last(chunks), chunkSize)],
   ].map((events) => ({ clef, timeSig, keySig, events }));
+}
+
+/**
+ * Set the time signature of `bars[i]` to `ts`. \
+ * All subsequent bars, until one with a different time signature comes, \
+ * will get the time signature. \
+ * The bars will be chunked again, and events will land in different bars. \
+ * If updating a time signature to a shorter one, extra bars will be added \
+ * at the end of the score to make room for the events. Ex:
+ * 
+ *  ```
+ *    bars = [{ 4/4: 4 2 4t }, { 4/4: 4 4 2 }]
+ * 
+ *    setTimeSignature(bars, 1, 2/4)
+ *    // bars = [{ 4/4: 4, 2, 4t }, { 2/4: 4 4 }, { 2/4: 2 }]
+ * 
+ *    setTimeSignature(bars, 0, 3/4)
+ *    // bars = [{ 3/4: 4 2 }, { 2/4: 2 }, { 2/4: 4 4t }, { 2/4: 4 4p }]
+ * 
+ * where
+ *    t: event.tiedToNext and
+ *    p: pause
+ *  ```
+ */
+export function setTimeSignature(bars: Bar[], i: number, ts: TimeSignature): Bar[] {
+  throw new Error("Not implemented");
 }
